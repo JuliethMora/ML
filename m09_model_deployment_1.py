16c5ebea3db54bd0a27f60506140b8b05acb6783
Juli M <92933505+JuliethMora@users.noreply.github.com>
2025-04-30 13:51:49 -0500

    Update m09_model_deployment_1.py

--- m09_model_deployment_1.py
+++ m09_model_deployment_1.py
@@ -3,7 +3,7 @@ def predict_popularity(url):
 
     reg = gdown.download('https://drive.google.com/file/d/1oy3QMpwGA23a_Aeg2DAxAIRAu5oztWCm/view?usp=drive_link','stack_model.pkl', 
                     quiet = False)
-    #reg = joblib.load('stack_model.pkl') 
+    reg = joblib.load('stack_model.pkl') 
 
     url_ = pd.DataFrame([url], columns=['song'])
 
