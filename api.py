69a3405b8cf9bb97acce38c5d88be099da4d3bec
Juli M <92933505+JuliethMora@users.noreply.github.com>
2025-04-29 19:38:42 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 # Importación librerías
 from flask import Flask
 from flask_restx import Api, Resource, fields
@@ -6,41 +12,35 @@ from m09_model_deployment_1 import predict_popularity
 
 app = Flask(__name__)
 
+# Cargar modelo al inicio para evitar recarga continua
+reg = joblib.load('stack_model.pkl')
+
+
 # Definición API Flask
 api = Api(
     app, 
     version='1.0', 
-    title='popularity song Prediction API',
-    description = 'popularity song Prediction API')
+    title='Song Popularity Prediction API',
+    description='Predicts the popularity of a song based on audio features.'
+)
 
-ns = api.namespace('predict', 
-     description='popularity')
+ns = api.namespace('predict', description='popularity')
 
 # Definición argumentos o parámetros de la API
 parser = ns.parser()
-parser.add_argument(
-    'URL', 
-    type=str, 
-    required=True, 
-    help='song to be analyzed', 
-    location='args')
+parser.add_argument('danceability', type=float, required=True, help='Danceability score of the song', location='args')
 
 resource_fields = api.model('Resource', {
     'result': fields.String,
 })
 
-# Definición de la clase para disponibilización
 @ns.route('/')
-class PhishingApi(Resource):
+class PopularityApi(Resource):
 
     @ns.doc(parser=parser)
     @ns.marshal_with(resource_fields)
     def get(self):
         args = parser.parse_args()
-        
-        return {
-         "result": prob(args['URL'])
-        }, 200
-    
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)    
+        danceability = args['danceability']
+        result = predict_popularity(danceability)
+        return {"result": result}, 200
