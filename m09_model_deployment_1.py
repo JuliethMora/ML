07849cee0ee0e6c030669bca7ad6ac492c8e67ea
Juli M <92933505+JuliethMora@users.noreply.github.com>
2025-04-30 16:26:16 -0500

    Update m09_model_deployment_1.py

--- m09_model_deployment_1.py
+++ m09_model_deployment_1.py
@@ -1,4 +1,9 @@
 import gdown
+import joblib
+import pandas as pd
+import sys
+import os
+
 # Función para transformar el parametro URL en el formato esperado por el modelo
 def predict_popularity(url):
 
@@ -9,20 +14,20 @@ def predict_popularity(url):
     url_ = pd.DataFrame([url], columns=['song'])
 
         
-    duration_ms          = pd.read_csv('duration_ms.csv')
-    explicit             = pd.read_csv('explicit.csv')
-    danceability         = pd.read_csv('danceability.csv')
-    energy               = pd.read_csv('energy.csv')
-    key                  = pd.read_csv('key.csv')
-    loudness             = pd.read_csv('loudness.csv')
-    mode                 = pd.read_csv('mode.csv')
-    speechiness          = pd.read_csv('speechiness.csv')
-    acousticness         = pd.read_csv('acousticness.csv')
-    track_genre_encoded  = pd.read_csv('track_genre_encoded.csv')
+    duration_ms = pd.read_csv('duration_ms.csv')
+    explicit = pd.read_csv('explicit.csv')
+    danceability = pd.read_csv('danceability.csv')
+    energy = pd.read_csv('energy.csv')
+    key = pd.read_csv('key.csv')
+    loudness = pd.read_csv('loudness.csv')
+    mode = pd.read_csv('mode.csv')
+    speechiness = pd.read_csv('speechiness.csv')
+    acousticness = pd.read_csv('acousticness.csv')
+    track_genre_encoded = pd.read_csv('track_genre_encoded.csv')
 
 
 
-    dumm_cols = [ 'duration_ms',
+    dumm_cols = ['duration_ms',
        'explicit', 'danceability', 'energy', 'key', 'loudness', 'mode',
        'speechiness', 'acousticness', 'instrumentalness', 'liveness',
        'valence', 'tempo', 'time_signature', 'track_genre_encoded']
